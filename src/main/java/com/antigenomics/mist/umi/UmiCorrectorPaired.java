5047a5ecf9d7773074fe19e0e3929fb1b8b39db2
mikessh <mikhail.shugay@gmail.com>
2016-02-08 18:26:17 +0300

    2prev

--- src/main/java/com/antigenomics/mist/umi/UmiCorrectorPaired.java
+++ src/main/java/com/antigenomics/mist/umi/UmiCorrectorPaired.java
@@ -16,7 +16,6 @@
 package com.antigenomics.mist.umi;
 
 import cc.redberry.pipe.OutputPort;
-import com.antigenomics.mist.misc.HeaderUtil;
 import com.milaboratory.core.io.sequence.PairedRead;
 import com.milaboratory.core.io.sequence.SingleReadImpl;
 
--- src/main/java/com/antigenomics/mist/umi/UmiCorrectorSingle.java
+++ src/main/java/com/antigenomics/mist/umi/UmiCorrectorSingle.java
@@ -16,8 +16,6 @@
 package com.antigenomics.mist.umi;
 
 import cc.redberry.pipe.OutputPort;
-import com.antigenomics.mist.preprocess.HeaderUtil;
-import com.milaboratory.core.io.sequence.PairedRead;
 import com.milaboratory.core.io.sequence.SingleRead;
 import com.milaboratory.core.io.sequence.SingleReadImpl;
 
